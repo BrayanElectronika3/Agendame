e2893e6ac776723a5ab0c17824a5dd852bde823f
Brayan Electronika 3 <brayanelectronika03@gmail.com>
2024-11-20 17:05:38 -0500

    Sidebar left shadcn

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Layout from './app/layout'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Layout></Layout>     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,68 @@
+import { Calendar, Home, Inbox, Settings, Calendar1Icon } from "lucide-react"
+
+import { AppUser } from "@/components/app-user"
+import { AppSwitcher } from "@/components/app-switcher"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarHeader,
+  SidebarFooter,
+} from "@/components/ui/sidebar"
+
+// Header and Fotter
+const data = {
+  user: { name: "User 123", email: "user123@example.com", avatar: "user.svg", },
+  teams: [
+    { name: "AGENDAME", logo: Calendar1Icon, plan: "Premium", },
+  ],
+}
+
+// Menu items.
+const items = [
+  { title: "Inicio", url: "#", icon: Home, },
+  { title: "Notificaciones", url: "#", icon: Inbox, },
+  { title: "Calendario", url: "#", icon: Calendar, },
+  { title: "Configuracion", url: "#", icon: Settings, },
+]
+ 
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible={"icon"}>
+      {/* Header */}
+      <SidebarHeader>
+        <AppSwitcher apps={data.teams}/>
+      </SidebarHeader>
+      {/* Content */}
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Aplicación</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      {/* Footer */}
+      <SidebarFooter>
+        <AppUser user={data.user}/>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
